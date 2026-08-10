2d1c4ebf157c23ac878df6f727e39c45a5ed7da6
Vishwajeet Singh Jii <vishwajeetsinghbinay@gmail.com>
2026-08-10 21:51:51 +0530

    Time: 1 ms (93.75%), Space: 45.7 MB (20.31%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int singleSum = 0;
+        int doubleSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] - 10 < 0) {
+                singleSum += nums[i];
+            } else {
+                doubleSum += nums[i];
+            }
+        }
+        if (singleSum != doubleSum) {
+            return true;
+        }
+        return false;
+    }
+}
